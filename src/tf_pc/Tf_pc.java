6585101f7134ec6000012cedfdb48850afecbb4a
Dayana <sophia.paz.alejos@gmail.com>
2024-06-25 17:13:00 -0500

    first commit

---
+++ src/tf_pc/Tf_pc.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package tf_pc;
+
+/**
+ *
+ * @author Dayana
+ */
+public class Tf_pc {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
